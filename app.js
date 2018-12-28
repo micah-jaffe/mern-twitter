02c47f21d69c6936f5f224a4d5633e2e61471b0a
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-28 14:25:58 -0800

    Add users and tweets routes

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 const app = express();
 const db = require('./config/keys').mongoURI;
 
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+
+const bodyParser = require("body-parser");
+
 mongoose
   .connect(
     db,
@@ -12,7 +17,11 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("bump"));
+app.get("/", (req, res) => res.send("hello"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
