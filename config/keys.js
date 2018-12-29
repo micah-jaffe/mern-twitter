7fb094fb81090102950f2564809c1cdb38add11f
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-29 15:23:20 -0800

    Add User model

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb://<micahjaffe>:<Darknecro95>@ds117423.mlab.com:17423/micah_db"
+  mongoURI: "mongodb://micahjaffe:Darknecro95@ds117423.mlab.com:17423/micah_db"
 };
