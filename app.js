03aa6de1ee71f2419d056a95707ea2bd605183b7
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-29 15:38:33 -0800

    Add passport

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const tweets = require('./routes/api/tweets');
 
 const bodyParser = require("body-parser");
 
+const passport = require("passport");
+
+
 mongoose
   .connect(
     db,
@@ -17,7 +20,9 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello"));
+// app.get("/", (req, res) => res.send("hello"));
+app.use(passport.initialize());
+require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
